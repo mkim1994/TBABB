293811c7a9cfd5e757375f16433e5c17a89948b8
Unknown <mkim2@andrew.cmu.edu>
2018-01-22 17:07:37 -0500

    additional setup

---
+++ Assets/Scripts/SpriteActor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class SpriteActor : MonoBehaviour {
+
+
+  //  public DialogueRunner dr;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        RotateSpriteTowardPlayer();
+    }
+
+    void RotateSpriteTowardPlayer(){
+        transform.LookAt(Services.GameManager.player.transform);
+        transform.eulerAngles += new Vector3(0, 180f, 0);
+    }
+
+}
