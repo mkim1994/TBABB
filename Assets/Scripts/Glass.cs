c6b6934660fe941c4d7da0331ac40ac3f639154d
DESKTOP-AV91D2I\paosa <paosalcedo@gmail.com>
2018-01-23 19:51:47 -0500

    Added Pickupable, Bottle, Glass scripts

---
+++ Assets/Scripts/Glass.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glass : Pickupable {
+	public bool hasLiquid;
+	public bool isFull;
+	public bool isDirty;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
