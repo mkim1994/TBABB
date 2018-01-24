63fc3d0ff24fcd45817653328adadf501b60f995
DESKTOP-AV91D2I\paosa <paosalcedo@gmail.com>
2018-01-24 17:52:16 -0500

    Movement and mouselook remapped to Rewired

--- Assets/Scripts/Pickupable.cs
+++ Assets/Scripts/Pickupable.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class Pickupable : MonoBehaviour {
+    public void TweenToPlayer(){
+		transform.SetParent(Services.GameManager.player.transform);
+		transform.DOLocalMove(Vector3.forward, 0.25f, false); 
+	}
 
+    public void DropThis(){
+
+    }
 	
 }
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -8,7 +8,7 @@ public class PlayerInput : MonoBehaviour {
 	
 	[SerializeField]float moveSpeed = 2.0f;
 	[SerializeField]float lookSensitivity = 0.01f;
-	[SerializeField]float smoothing = 2.0f;
+	// [SerializeField]float smoothing = 2.0f;
 
 	Vector2 smoothV;
 
@@ -16,9 +16,13 @@ public class PlayerInput : MonoBehaviour {
 	private Vector2 lookVector;
 	public int playerId = 0; 
 	private Player player;
-	
 	private CharacterController cc;
 	private Camera myCam;
+	public LayerMask layerMask;
+	Pickupable pickupable;
+	private float maxInteractionDist = 4f;
+
+	bool in_pickUpLeft;
 
 	void Awake(){
 		Cursor.visible = false;
@@ -38,26 +42,56 @@ public class PlayerInput : MonoBehaviour {
 		moveVector.z = player.GetAxis("Move Vertical");
 		lookVector.x = player.GetAxis("Look Horizontal");
 		lookVector.y = player.GetAxis("Look Vertical");
+		in_pickUpLeft = player.GetButtonDown("Pick Up Left");
+
 		// Debug.Log(lookVector.x + " " + lookVector.y);
 	}
 
-	private void ProcessInput(){
-		if(moveVector.x != 0.0f || moveVector.y != 0.0f) {
-            cc.Move(moveVector * moveSpeed * Time.deltaTime);
-        }
+	private void ProcessInput(){		
+		#region Movement
+		//which direction you'll move in
+		Vector3 moveDir = new Vector3 (moveVector.x, 0, moveVector.z);
 
+		//Speed is directed towards where you're facing.
+		moveDir = transform.rotation * moveDir;
+
+		cc.Move (moveDir * moveSpeed * Time.deltaTime);
+		#endregion
+
+		#region MouseLook
 		lookVector.y = Mathf.Clamp (lookVector.y, -90f, 90f);
 
 		myCam.transform.Rotate (-lookVector.y * lookSensitivity, 0, 0f);
 		cc.transform.Rotate (0, lookVector.x * lookSensitivity, 0);
+		#endregion
+		
+		#region Pick Up Left
+		if(in_pickUpLeft){
+ 			Debug.Log("Picking up with left hand");
+			if(pickupable != null){
+				pickupable.TweenToPlayer();
+			}
+		}       
+		#endregion 
 
-		// myCam.transform.localRotation = Quaternion.AngleAxis(-lookVector.y * lookSensitivity, Vector3.right); //look up/down
-		// transform.localRotation = Quaternion.AngleAxis(lookVector.x * lookSensitivity, Vector3.up); //look left/right
+	}
 
-		// myCam.transform.Rotate (-Input.GetAxis ("Mouse Y"), Input.GetAxis ("Mouse X"), 0f);
+	private void InteractionRay(){
+		Ray ray = new Ray(transform.position, transform.forward);
+		float rayDist = Mathf.Infinity;
+		RaycastHit hit = new RaycastHit();
 
-		// 	//unroll our camera
-		// myCam.transform.localEulerAngles = new Vector3 (myCam.transform.localEulerAngles.x, myCam.transform.localEulerAngles.y, 0f); 
+		if(Physics.Raycast(ray, out hit, rayDist, layerMask)){
+			GameObject hitObj = hit.transform.gameObject; //if you're actually looking at something
+ 			
+			if(hitObj.GetComponent<Pickupable>() != null && Vector3.Distance(transform.position, hitObj.transform.position) <= maxInteractionDist){ //check if object looked at can be picked up
+				pickupable = hitObj.GetComponent<Pickupable>(); //if it's Pickupable and close enough, assign it to pickupable.				  
+ 			} 		
+		} else { //no hit
+			pickupable = null; //if you're not looking at anything, make this null
+ 		}
+
+
 	}
 
 }
