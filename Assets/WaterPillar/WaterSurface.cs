754175c67830d75a018e152d5d08aeddd58ae886
pao_s <paosalcedo@gmail.com>
2018-04-19 15:04:13 -0400

    Added Kevin's water surface script to repo

---
+++ Assets/WaterPillar/WaterSurface.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class WaterSurface : MonoBehaviour {
+ 	// Use this for initialization
+	void Update () {
+		transform.rotation = Quaternion.identity;
+	}
+}
