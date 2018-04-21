27357400bb89cdee4afe18887f7345474ab3a996
pao_s <paosalcedo@gmail.com>
2018-04-21 04:31:49 -0400

    Fixed backroom colliders; you can now pick up pen and notepad, but no interactions yet

--- Assets/Scripts/Notepad.cs
+++ Assets/Scripts/Notepad.cs
@@ -2,20 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Notepad : MonoBehaviour {
+public class Notepad : Pickupable {
 
     public Texture[] notes;
     public Texture notesigned;
     Material mat;
     bool onlyOnce;
 	// Use this for initialization
-	void Start () {
+	protected override void Start () {
+        base.Start();
         onlyOnce = false;
         mat = GetComponent<MeshRenderer>().material;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	public override void Update () {
         if(mat.GetTexture("_MainTex") != notes[Services.GameManager.dayManager.currentDay]){
             if (Services.GameManager.dayManager.currentDay != 0)
             {
